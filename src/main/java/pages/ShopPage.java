a014becd68dbdbb49c98386786e9b26ea83c6d1d
mahmoudyehia664 <mahmoudyehia664@gmail.com>
2024-10-05 01:39:43 +0300

    delete filter,edit shop page

--- src/main/java/pages/ShopPage.java
+++ src/main/java/pages/ShopPage.java
@@ -3,41 +3,7 @@ package pages;
 import org.openqa.selenium.WebDriver;
 
 public class ShopPage extends AbstractShopAndCategoryPage {
-//    public Filter filter;
     public ShopPage(WebDriver driver){
         super(driver);
-//        filter=new Filter(driver);
     }
-//    public ShopPage applyPriceFilter(int minPrice,int maxPrice){
-////        filter.applyPriceFilter(minPrice,maxPrice);
-//        new Filter(driver).applyPriceFilter(minPrice,maxPrice);
-//        return this;
-//    }
-//    public ShopPage applyColorFilter(String color){
-//        new Filter(driver).applyColorFilter(color);
-////        filter.applyColorFilter(color);
-//        return this;
-//    }
-//    public int getProductCountForAColor(String color){
-//        return new Filter(driver).getColorCount(color);
-//    }
-//    public ShopPage applyBrandFilter(String brand){
-////        filter.applyBrandFilter(brand);
-//        new Filter(driver).applyBrandFilter(brand);
-//        return this;
-//    }
-//    public int getProductCountForABrand(String brand){
-//        return new Filter(driver).getBrandCount(brand);
-//    }
-//    public String getAppliedFilters(){
-//        return new Filter(driver).getAppliedFilters();
-//    }
-//    public ShopPage clearPriceFilter(){
-//        new Filter(driver).clearPriceFilter();
-//        return this;
-//    }
-//    public ShopPage clearPriceFilter(String text){
-//        new Filter(driver).clearPriceFilter(text);
-//        return this;
-//    }
 }
